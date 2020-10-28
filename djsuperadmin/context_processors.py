5817d8e46feeec757cc874485690e02600036a7c
bnznamco <gabriele.baldi.01@gmail.com>
2020-10-28 19:39:06 +0100

    reduce db query impact for multiple content in single page

--- djsuperadmin/context_processors.py
+++ djsuperadmin/context_processors.py
@@ -1,16 +1,18 @@
 from django.utils.safestring import mark_safe
 import os
 from django.urls import reverse
+from djsuperadmin.models import Content
 
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 def djsuperadmin(request):
+    context = {"djsa-contents": list(Content.objects.language().fallbacks().all()), "djsuperadminjs": ""}
     if request.user.is_authenticated and request.user.is_superuser:
-        with open(os.path.join(BASE_DIR, 'dist/djsuperadmin.bundle.js'), 'r') as js_file:
+        with open(os.path.join(BASE_DIR, "dist/djsuperadmin.bundle.js"), "r") as js_file:
             js = "<script>var djsa_logout_url='{0}';{1}</script>".format(
-                reverse('account:logout', current_app=request.resolver_match.namespace),
-                js_file.read())
-        return {'djsuperadminjs': mark_safe(js)}
-    return {'djsuperadminjs': ''}
+                reverse("account:logout", current_app=request.resolver_match.namespace), js_file.read()
+            )
+        context.update({"djsuperadminjs": mark_safe(js)})
+    return context
--- djsuperadmin/templatetags/djsuperadmintag.py
+++ djsuperadmin/templatetags/djsuperadmintag.py
@@ -7,19 +7,19 @@ from django.utils.html import escape
 def _get_span(editor_mode, content):
     if editor_mode == 0:
         content.content = escape(content.content)
-    return '<span class="djsuperadmin" data-mode="{0}" data-djsa ="{1}">{2}</span>'.format(
-            str(editor_mode), str(content.id), content.content)
+    return '<span class="djsuperadmin" data-mode="{0}" data-djsa ="{1}">{2}</span>'.format(str(editor_mode), str(content.id), content.content)
 
 
 def _get_content(context, identifier, editor_mode, placeholder="New content"):
-    content, created = Content.objects.language().fallbacks().get_or_create(identifier=identifier)
-    if created:
-        content.content = placeholder
-        content.save()
-    if context['request'].user.is_superuser:
+    content = (
+        context["djsa-contents"]
+        and next((c for c in context["djsa-contents"] if c.identifier == identifier), None)
+        or Content.objects.language().fallbacks().create(identifier=identifier, content=placeholder)
+    )
+    if context["request"].user.is_superuser:
         return mark_safe(_get_span(editor_mode, content))
     else:
-        return content.content if editor_mode==0 else mark_safe(content.content)
+        return content.content if editor_mode == 0 else mark_safe(content.content)
 
 
 register = template.Library()
